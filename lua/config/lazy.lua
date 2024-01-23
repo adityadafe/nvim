2ffe3cd2dff1c9b7e1858a6b7877d481806b8686
kakashi1210 <desmondaditya@gmail.com>
2024-01-24 01:09:21 +0530

    feat: some basic keymaps

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,7 +9,9 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
+      colorscheme = "solarized-osaka",
+    } },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
