008b71445578fa618ad9caa3a29f718cde3fca99
kakashi1210 <desmondaditya@gmail.com>
2024-01-28 22:37:16 +0530

    feat: added cowboy for discipline

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local discipline = require("kakashifr.discipline")
+discipline.cowboy()
+
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
